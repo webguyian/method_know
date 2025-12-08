b628704556db68b8c5991c6b53e9cc7c16720305
Ian McPhail <ian.mcphail@method.com>
2025-12-08 13:53:29 -0500

    Add user authentication

--- lib/method_know_web/router.ex
+++ lib/method_know_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MethodKnowWeb.Router do
   use MethodKnowWeb, :router
 
+  import MethodKnowWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule MethodKnowWeb.Router do
     plug :put_root_layout, html: {MethodKnowWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule MethodKnowWeb.Router do
       live_dashboard "/dashboard", metrics: MethodKnowWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", MethodKnowWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MethodKnowWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", MethodKnowWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{MethodKnowWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule MethodKnow.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.8.2"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
