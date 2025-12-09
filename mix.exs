76a5cda2e5def8ba810d26dd9295e23b9a9ecd84
Ian McPhail <ian.mcphail@method.com>
2025-12-08 21:46:09 -0500

    Add Lucide icons

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule MethodKnow.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:lucide_live_view, "~> 0.1.0"}
     ]
   end
 
