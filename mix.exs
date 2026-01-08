ab6636162849f261b08291aee09aaf45cb36b4a2
Ian McPhail <ian.mcphail@method.com>
2026-01-08 16:54:50 -0500

    Bump project version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MethodKnow.MixProject do
   def project do
     [
       app: :method_know,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
